b62f73d16f42d24c2fd37bcbca7024445b7d0a36
Kr0nZ <matthyo@gmail.com>
2013-02-06 04:01:50 -0500

    merged files, added common window+control class

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import xbmc
 import xbmcgui
 import xbmcaddon
-from resources.lib.windowGUI import windowGUI
+from resources.lib.windowGUI import WindowGUI
 from service import CronXbmc, CronJob
 
 __addon_id__ = "service.cronxbmc"
@@ -9,8 +9,7 @@ __Addon__ = xbmcaddon.Addon(__addon_id__)
 __cwd__        = __Addon__.getAddonInfo('path')
 
 def updateJobs():
-    ui = windowGUI( "script-cron-main.xml" , __cwd__, "Default")
-    ui.doModal()
+    ui = WindowGUI( __cwd__)
     del ui
     
 
