2a1baaef5a370a710d9696f744cd6018a649bd0b
Termo <termopeten@gmail.com>
2022-04-09 11:34:13 +0200

    revert info logging

--- resources/lib/cron_utils/utils.py
+++ resources/lib/cron_utils/utils.py
@@ -19,12 +19,13 @@ def addon_dir():
     return __Addon.getAddonInfo('path')
 
 
-def log(message, loglevel=xbmc.LOGINFO):
+def log(message, loglevel=xbmc.LOGDEBUG):
     xbmc.log(__addon_id__ + "-" + __Addon.getAddonInfo('version') + " : " + message, level=loglevel)
 
 
 def showNotification(title, message):
-    xbmcgui.Dialog().notification(getString(30000), message, time=4000, icon=xbmcvfs.translatePath(__Addon.getAddonInfo('path') + "/resources/images/icon.png"), sound=False)
+    xbmcgui.Dialog().notification(getString(30000), message, time=4000, icon=xbmcvfs.translatePath(
+        __Addon.getAddonInfo('path') + "/resources/images/icon.png"), sound=False)
 
 
 def setSetting(name, value):
